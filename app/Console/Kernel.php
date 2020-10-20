6927c76448c3ecbfb32febfb5c4335a73a0bd48c
Diego <diegonskt@gmail.com>
2020-10-20 15:04:48 +0200

    fix error

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //Command\Calculadora::class
+        Command\Calculadora::class,
     ];
 
     /**
